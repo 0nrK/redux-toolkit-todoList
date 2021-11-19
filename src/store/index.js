71d0aaadae8fe6bf446171faa46cecc44820be5d
0nrK <onurkirnapci@gmail.com>
2021-11-19 13:19:13 +0300

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "../features/toDoSlice"
-
+//store
 export const store = configureStore({
     reducer: todosReducer
 });
